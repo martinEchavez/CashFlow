57b250efec9f3db656896b09b71da2af12e2eb5f
Martin Echavez <martinechavez1994@gmail.com>
2024-03-03 10:47:02 -0500

    Add SplashScreen component

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue';
+import SplashScreen from '@/components/SplashScreen.vue';
+
+const Home = defineAsyncComponent(() =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(import('./components/Home.vue'))
+    }, 2500)
+  })
+)
+</script>
+
+<template>
+  <Suspense>
+    <template #default>
+      <Home />  
+    </template>
+    <template #fallback>
+      <SplashScreen />
+    </template>
+  </Suspense>
+</template>
+
+<style scoped>
+  html,
+  body,
+  .app {
+    min-height: 100vh;
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  * {
+    --brand-green: #04b500;
+    --brand-blue: #0689b0;
+  }
+</style>
