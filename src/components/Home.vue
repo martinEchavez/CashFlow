ba778305b318736b1d398ebb85a8d896d385d828
Martin Echavez <martinechavez1994@gmail.com>
2024-03-11 20:55:40 -0500

    Add Resume and Movements component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,30 @@
 <script setup lang="ts">
-import Layout from '@/components/Layout.vue';
-import Header from '@/components/Header.vue';
+import Layout from './Layout.vue';
+import Header from './Header.vue';
+import Resume from './Resume/index.vue'
+import Movements from './Movements.vue';
+import { ref, type Ref } from 'vue';
+
+const amount: Ref<number | undefined> = ref();
+const date: Ref<Date | undefined> = ref( new Date(2024, 2, 11));
+
 </script>
 
 <template>
     <Layout>
         <template #header>
             <Header></Header>
+        </template>
+        <template #resume>
+            <Resume
+                :label="'Ahorro total'"
+                :date="date"
+                :amount=amount
+                :total-amount="2000000"
+            />
+        </template>
+        <template #movements>
+            <Movements />
         </template>
     </Layout>
 </template>
---
+++ src/components/Resume/index.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{
+    label: string,
+    date?: Date,
+    amount?: number,
+    totalAmount: number
+}>();
+
+const amountVisual = computed(() => props.amount ? props.amount : props.totalAmount);
+const labelVisual = computed(() => props.date ? props.date.toDateString() : props.label);
+
+</script>
+
+<template>
+    <main>
+        <p>{{ labelVisual }}</p>
+        <h1>{{ amountVisual }}</h1>
+    </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+}
+h1,
+p {
+  margin: 0;
+  text-align: center;
+}
+h1 {
+  margin-top: 14px;
+  color: var(--brand-green);
+}
+.graphic {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 48px 24px;
+  box-sizing: border-box;
+}
+</style>
